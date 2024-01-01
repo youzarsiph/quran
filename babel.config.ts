242dc7fc2a20469fbf5691ae6f89b60bdfe9fab9
Yousef Abu Shanab <josephyousef249@gmail.com>
2024-01-01 09:05:38 -0800

    Update .gitignore and remove .expo from repo

--- babel.config.ts
+++ babel.config.ts
@@ -1,11 +1,10 @@
-module.exports = function (api) {
+module.exports = function (api: { cache: (arg0: boolean) => void }) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
     plugins: [
       // Required for expo-router
       "expo-router/babel",
-      "nativewind/babel",
     ],
   };
 };
